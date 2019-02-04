02409fe1291f89182e6a0fd007f72abdfe040612
unknown <38929577+meginks@users.noreply.github.com>
2019-02-04 16:00:29 -0600

    got styles to shift when different menu items are clicked - still working on getting correct content to appear

--- js/script.js
+++ js/script.js
@@ -4,29 +4,27 @@ class MenuLink {
         this.data = this.element.dataset.menu;
         this.item = document.querySelector(`.menu-item[data-menu='${this.data}']`);
         this.menuItem = new MenuItem(this.item);
-        this.element.addEventListener('click', () => this.select());
+        this.element.addEventListener('click', () => { this.select()});
     };
     select() {
-        const links = document.querySelectorAll('.menu-link');
-        links.forEach( link => link.classList.remove('menu-link-selected'));
-        this.linkElement.classList.add('menu-link-selected');
-        this.menuItem.choose();
+        const menuLinks = document.querySelectorAll('.menu-link');
+        menuLinks.forEach( link => link.classList.remove('menu-link-selected'));
+        this.element.classList.add('menu-link-selected');
+        this.MenuItem.choose();
     }
 }
-
 class MenuItem {
     constructor(element) {
         this.element = element;
     }
     choose() {
         const items = document.querySelectorAll('.menu-item');
-        items.forEach(item => {item.classList.remove('menu-item-selected');
-    });
-        this.menuElement.classList.add('menu-item-selected');
+        items.forEach(item => item.classList.remove('menu-item-selected'));
+        this.element.classList.add('menu-item-selected');
     }
 }
 
 let links = document.querySelectorAll('.menu-link');
 links.forEach(link => new MenuLink(link));
 
-console.log("is this working?")
+ console.log(links);
